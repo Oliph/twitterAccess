7df84e8508fa88aaf6d6e825bb1379ccbe1f1a58
Olivier Philippe <olivier.philippe@gmail.com>
2020-05-02 18:16:01 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="twitterAccess",
+    version="1.0",
+    description="Collection of developed tools to access twitter API",
+    author="Olivier Philippe",
+    author_email="olivier.philippe@gmail.com",
+    packages=["twitterAccess"],
+)
